bc19d5faeb122c7ecc9e278632d1d777548c0140
Jonathan Jackson <jnthjackson@gmail.com>
2020-09-21 03:26:55 -0400

    First part of the tutorial done. Static Triangle with multiple images in flight  in the swapchain (controlled)

---
+++ VulkanTuto/VulkanTuto.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <fstream>
+
+static std::vector<char> readFile(const std::string& filename) {
+    std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+    if (!file.is_open()) {
+        throw std::runtime_error("failed to open file!");
+    }
+}
